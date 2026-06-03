a8c3a9f16d8790deb5093f9d4eea841592908157
MohamedElMustafa123 <nobimohamed262@gmail.com>
2026-06-03 21:33:36 +0400

    Used modules and imported cart from cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,5 @@
+import {cart} from '../data/cart.js';
+
 // This will store ALL the HTML for every product as one big string
 // We start empty and keep adding to it inside the loop
 let productsHTML = '';
