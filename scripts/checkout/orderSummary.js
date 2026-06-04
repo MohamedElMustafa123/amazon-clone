eb7f3ba20b4373b1098b517ca2ae7672d5ca0b1a
MohamedElMustafa123 <nobimohamed262@gmail.com>
2026-06-04 17:16:22 +0400

    Made payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,18 +4,15 @@ import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
+import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 
-hello();
-const today = (dayjs());
 
-const deliveryDate = today.add(7, 'days');
 
 export function renderOrderSummary(){
   let cartSummaryHTML = '';
 
-  deliveryDate.format('dddd, MMMM D');
 
   cart.forEach((cartItem) =>{
 
@@ -138,6 +135,8 @@ export function renderOrderSummary(){
 
       const container = document.querySelector(`.js-cart-item-container-${productId}`);
       container.remove();
+
+      renderPaymentSummary();
     });
   });
 
@@ -146,6 +145,7 @@ export function renderOrderSummary(){
       const {productId, deliveryOptionId} = element.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
 
     })
 
